d6900896e668b366885b3d11a828df3ac44e92c0
Abner Rizzi <abner.rizzi@gmail.com>
2023-08-07 16:36:53 -0300

    reverting unrelated changes

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from app.extensions import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column()
     login = db.Column()
     password  = db.Column()
