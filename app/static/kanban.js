d0abaa859c5c20f45b6e330e30cc3db287dc6729
Abner Rizzi <abner.rizzi@gmail.com>
2023-08-07 17:56:02 -0300

    Implementing job module kanban

---
+++ app/static/kanban.js
@@ -0,0 +1,66 @@
+
+// Drag and Drop Functions
+function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drag(event) {
+    event.currentTarget.classList.add("dragging");
+    taskId = $(event.target).closest(".card.mb-3").attr("data-task-id");
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    var targetColumnId = event.currentTarget.getAttribute("data-column-id");
+    var targetColumn = $(event.currentTarget);
+
+    // Move the dragged task to the target column
+    var task = $("[data-task-id='" + taskId + "']");
+    targetColumn.append(task);
+
+    // Update the task's data-column-id attribute
+    task.attr("data-column-id", targetColumnId);
+
+    // Perform AJAX request to update the job status
+    $.ajax({
+        url: "/jobs/update_status/" + taskId,
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            new_status_id: targetColumnId,
+        }),
+        success: function(response) {
+          console.log("Job status updated successfully.");
+        },
+        error: function(xhr, status, error) {
+            console.error("An error occurred while updating the job status:", error);
+        }
+    });
+
+  // Reset the taskId variable
+    taskId = null;
+  }
+
+  $(document).ready(function() {
+    $(".card.mb-3").on("dblclick", function(event) {
+        var taskId = $(this).attr("data-task-id");
+        window.location.href = "/jobs/edit/" + taskId;
+    });
+
+      // Add dragend event listener to remove 'dragging' class
+    $(".card.mb-3").on("dragend", function(event) {
+      event.currentTarget.classList.remove("dragging");
+    });
+
+    // Set draggable property for cards with class "card mb-3"
+    $(".card.mb-3").each(function() {
+      $(this).attr("draggable", "true");
+    });
+
+    // Add dragstart event listener to cards
+    $(".card.mb-3").on("dragstart", drag);
+
+    // Add dragover and drop event listeners to columns
+    $(".card[data-column-id]").on("dragover", allowDrop);
+    $(".card[data-column-id]").on("drop", drop);
+  });
