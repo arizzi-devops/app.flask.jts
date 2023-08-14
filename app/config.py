abd46f2769cc05cb6755b1246dd5821eea2d6822
Abner Rizzi <abner.rizzi@gmail.com>
2023-08-14 10:03:25 -0300

    Adjusting environment settings

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     DB_USER = os.environ['DB_USER']
     DB_PASS = os.environ['DB_PASS']
     DB_HOST = os.environ['DB_HOST']
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://"+DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_HOST\
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://"+DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
